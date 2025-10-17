15cab9e632ccec37ddfdad86cc1d4953b782b8e1
Your Name <your-email@example.com>
2025-10-16 20:09:47 -0400

    Add missing path module import

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 
 // Import routes
 const webhookRoutes = require('./routes/webhooks');
